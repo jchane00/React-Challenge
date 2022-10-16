6fa0e5360bd574a3a551598917fc49616c1597f3
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-10-16 00:40:22 -0500

    updated Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,18 +1,27 @@
 import Project from "./Project"
+import muscleHustle from "../img/muscleHustle.png"
+import Cars2Go from "../img/Cars2Go.png"
+import freshly from "../img/fresh.ly.png"
 
 export default function Portfolio(){
     const items = [
         {
             title: "Cars2Go",
-            image:"https://drive.google.com/file/d/1kSvEkgb-367NinD1F7_B07ekpJv8QR9_/view?usp=sharing",
+            image: Cars2Go,
             liveLink:"https://safe-river-61972.herokuapp.com/" ,
             repoLink:"https://github.com/ashwinilh/Cars2Go/tree/ashwini"
         },
         {
             title: "fresh.ly",
-            image:"https://drive.google.com/file/d/1VU2odcrJUEFIKnISkFhRX_yRoUHwrLex/view?usp=sharing",
+            image: freshly,
             liveLink:"https://garcia2697.github.io/fresh.ly/" ,
             repoLink:"https://github.com/garcia2697/fresh.ly"
+        },
+        {
+            title: "Muscle Hustle",
+            image: muscleHustle,
+            liveLink:"https://musclehustle.herokuapp.com//" ,
+            repoLink:"https://github.com/jchane00/Fitness-App"
         },
    
     ]
